3e6a26cb7e4875d8a956da8fab716addd03ee3bd
kelly <kelly.n.waite@gmail.com>
2016-11-24 13:14:29 -0700

    Add asc and dsc sorting to table columns, Add jQuery CDN

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -5,18 +5,21 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
 import data from "./data.json"
 
+let order = 'desc';
+
 export default class App extends React.Component {
+
   render () {
     console.log(data);
     return (
     <div>
       <BootstrapTable className="table" data={ data }>
-       <TableHeaderColumn dataField='id' isKey>Id</TableHeaderColumn>
-       <TableHeaderColumn dataField='userName'>User Name Name</TableHeaderColumn>
-       <TableHeaderColumn dataField='postTitle'>Post Title</TableHeaderColumn>
-       <TableHeaderColumn dataField='views'>Views</TableHeaderColumn>
-       <TableHeaderColumn dataField='likes'>Likes</TableHeaderColumn>
-       <TableHeaderColumn dataField='date'>Date Created</TableHeaderColumn>
+       <TableHeaderColumn dataField='id' isKey={ true } dataSort={ true }>Id</TableHeaderColumn>
+       <TableHeaderColumn dataField='userName' dataSort={ true }>User Name Name</TableHeaderColumn>
+       <TableHeaderColumn dataField='postTitle' dataSort={ true }>Post Title</TableHeaderColumn>
+       <TableHeaderColumn dataField='views' dataSort={ true }>Views</TableHeaderColumn>
+       <TableHeaderColumn dataField='likes' dataSort={ true }>Likes</TableHeaderColumn>
+       <TableHeaderColumn dataField='date' dataSort={ true }>Date Created</TableHeaderColumn>
       </BootstrapTable>
 
     </div>
