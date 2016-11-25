8bda25e94a2f49ed68eae060b1faba4725adb24d
kelly <kelly.n.waite@gmail.com>
2016-11-25 13:51:41 -0700

    Clean up formatting so it is easier to see attributes of components

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 
-// import data from "./data.json"
+// import data from "./data.json" //Do not need because of firebase
 
 import AppSass from "./App.sass"
 
@@ -78,30 +78,58 @@ export default class App extends React.Component {
      } ],
      sizePerPage: 5,
      pageStartIndex: 1,
-     paginationSize: 5,  // the pagination bar size.
-     prePage: 'Prev', // Previous page button text
-     nextPage: 'Next', // Next page button text
-     firstPage: 'First', // First page button text
-     lastPage: 'Last', // Last page button text
-     paginationShowsTotal: this.renderShowsTotal  // Accept bool or function
-     // hideSizePerPage: true > You can hide the dropdown for sizePerPage
+     paginationSize: 5,
+     prePage: 'Prev',
+     nextPage: 'Next',
+     firstPage: 'First',
+     lastPage: 'Last',
+     paginationShowsTotal: this.renderShowsTotal
    };
+
     return (
     <div>
       <h1 className="header">Table</h1>
       <div className="center-block tableDiv table-responsive" >
-        <BootstrapTable className="tableDesign"
+        <BootstrapTable
+            className="tableDesign"
             data={ data }
             pagination={ true }
             options={ options }
-            insertRow={ true }
-            >
-         <TableHeaderColumn dataField='id' isKey={ true } dataSort={ true } width="60px">Id</TableHeaderColumn>
-         <TableHeaderColumn width="170px" dataField='userName' dataSort={ true } filter={ { type: 'TextFilter'} }>User Name</TableHeaderColumn>
-         <TableHeaderColumn dataField='postTitle' dataSort={ true }>Post Title</TableHeaderColumn>
-         <TableHeaderColumn dataField='views' dataSort={ true } width="80px">Views</TableHeaderColumn>
-         <TableHeaderColumn dataField='likes' dataSort={ true } width="80px">Likes</TableHeaderColumn>
-         <TableHeaderColumn dataField='date' dataSort={ true } width="100px">Date Created</TableHeaderColumn>
+            insertRow={ true }>
+         <TableHeaderColumn
+              dataField='id'
+              isKey={ true }
+              dataSort={ true } width="60px">
+            Id
+          </TableHeaderColumn>
+         <TableHeaderColumn
+              width="170px"
+              dataField='userName'
+              dataSort={ true }
+              filter={ { type: 'TextFilter'} }>
+            User Name
+        </TableHeaderColumn>
+         <TableHeaderColumn
+              dataField='postTitle'
+              dataSort={ true }>
+           Post Title
+         </TableHeaderColumn>
+         <TableHeaderColumn
+              dataField='views'
+              dataSort={ true } width="80px">
+            Views
+         </TableHeaderColumn>
+         <TableHeaderColumn
+              dataField='likes'
+              dataSort={ true } width="80px">
+            Likes
+         </TableHeaderColumn>
+         <TableHeaderColumn
+              dataField='date'
+              dataSort={ true }
+              width="100px">
+            Date Created
+         </TableHeaderColumn>
        </BootstrapTable>
      </div>
     </div>
